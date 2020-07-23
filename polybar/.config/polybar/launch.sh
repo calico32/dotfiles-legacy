93fdc08167708812d97857ea023b5b150978af77
wiisportsresort <devdoge1@gmail.com>
2020-07-22 22:05:00 -0400

    polybar: use env vars for system-specific things

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-## Add this to your wm startup file.
+# get env vars
+source "$HOME/.config/polybar/env.sh"
 
-# Terminate already running bar instances
+# terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
+# wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
-# Logfile
+# logfile
 date=$(date "+%Y-%m-%d_%H:%M:%S")
 logfile="$HOME/.config/polybar/logs/log-$date.log"
 
-# Launch bar1 and bar2
-polybar main -c ~/.config/polybar/main.ini > "$logfile" 2>&1 &
+polybar main -c "$HOME/.config/polybar/main.ini" > "$logfile" 2>&1 &
