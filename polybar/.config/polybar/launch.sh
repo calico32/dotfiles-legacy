589f09eac5aa776fe4651aab288dd426d3adcbb3
wiisportsresort <devdoge1@gmail.com>
2020-07-22 20:40:42 -0400

    log polybar output

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,5 +8,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
+# Logfile
+logfile="$HOME/.config/polybar/logs/log-$(date \"+%Y-%m-%d_%H:%M:%S\").log"
+
 # Launch bar1 and bar2
-polybar main -c ~/.config/polybar/main.ini &
+polybar main -c ~/.config/polybar/main.ini -l info > "$logfile" 2>&1 &
